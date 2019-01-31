8a72024eb215c89a271c806a15bd711a432e5d9f
Petko <skodap@gmail.com>
2019-01-31 20:15:56 +0100

    Added paging support

--- FunctionApp1/Api/Verses/Get.cs
+++ FunctionApp1/Api/Verses/Get.cs
@@ -1,10 +1,12 @@
 using FunctionApp1.ApiModels;
 using FunctionApp1.TableModels;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -13,8 +15,8 @@ namespace FunctionApp1.Api.Verses
 {
     public static class Get
     {
-        [FunctionName("Get-Verse")]
-        public static async Task<IActionResult> Run(
+        [FunctionName("Get-Verses")]
+        public static async Task<HttpResponseMessage> GetVerses(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "verses/{variant}")] HttpRequestMessage request,
             [Table("InfinitePoemV1")] CloudTable cloudTable,
             string variant,
@@ -22,21 +24,47 @@ namespace FunctionApp1.Api.Verses
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            var validVariant = new string[] { "en", "sk" }.Any(x => x == variant);
+            if (!ValidateLanguageVariant(variant))
+                return request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, "Please pass a valid language");
 
-            if (!validVariant)
-                return new BadRequestObjectResult("Please pass a valid language");
-
-            var query = new TableQuery<VerseEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, variant));
+            var query = new TableQuery<VerseEntity>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, variant))
+                .Take(2);
             var token = new TableContinuationToken();
             var queryResult = await cloudTable.ExecuteQuerySegmentedAsync(query, null);
+            var apiVerses = queryResult.Select(ToApiVerse).ToList();
 
-            var apiVerses = queryResult.ToList().Select(ToApiVerse);
+            var response = request.CreateResponse(System.Net.HttpStatusCode.OK, apiVerses);
+            response.Headers.Add("ContinuationToken", Newtonsoft.Json.JsonConvert.SerializeObject(queryResult.ContinuationToken));
 
-            // add continuation token as a header using action filter https://stackoverflow.com/questions/32017686/add-a-custom-response-header-in-apicontroller/52264433
-            // queryResult.ContinuationToken
+            return response;
+        }
 
-            return new OkObjectResult(apiVerses);
+        [FunctionName("Get-Verses-Next")]
+        public static async Task<HttpResponseMessage> GetVersesNext(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "verses/{variant}/next")] HttpRequestMessage request,
+            [Table("InfinitePoemV1")] CloudTable cloudTable,
+            string variant,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (!ValidateLanguageVariant(variant))
+                return request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, "Please pass a valid language");
+
+            var stringContent = await request.Content.ReadAsStringAsync();
+            var tableContinuationToken = JsonConvert.DeserializeObject<TableContinuationToken>(stringContent);
+
+            var query = new TableQuery<VerseEntity>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, variant))
+                .Take(2);
+            var queryResult = await cloudTable.ExecuteQuerySegmentedAsync(query, tableContinuationToken);
+            var apiVerses = queryResult.Select(ToApiVerse).ToList();
+
+            var response = request.CreateResponse(System.Net.HttpStatusCode.OK, apiVerses);
+            response.Headers.Add("ContinuationToken", JsonConvert.SerializeObject(queryResult.ContinuationToken));
+
+            return response;
         }
 
         public static VerseModel ToApiVerse(VerseEntity item)
@@ -45,6 +73,11 @@ namespace FunctionApp1.Api.Verses
             {
                 Text = item.Text,
             };
+        }
+
+        private static bool ValidateLanguageVariant(string variant)
+        {
+            return new string[] { "en", "sk" }.Any(x => x == variant);
         }
     }
 }
